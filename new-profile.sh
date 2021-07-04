e085ec756bc9af31b9f0d94972579b310e918b88
Phiaboo <kieri.burke@gmail.com>
2021-07-03 19:16:07 -0500

    better repo handling

--- new-profile.sh
+++ new-profile.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
-echo y | sudo add-apt-repository universe
-echo y | sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+
+#checking for repos
+UNIVERSE_EXIST=`egrep -v '^#|^ *$' /etc/apt/sources.list /etc/apt/sources.list.d/* | grep universe`
+PARTNER_EXIST=`egrep -v '^#|^ *$' /etc/apt/sources.list /etc/apt/sources.list.d/* | grep partner`
+CHROME_EXIST=`egrep -v '^#|^ *$' /etc/apt/sources.list /etc/apt/sources.list.d/* | grep chrome`
+
+if [ ! -z "$PARTNER_EXIST" ]
+then
+    echo partner repo already enabled
+else
+   echo y | sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+fi
+
+if [[ ! -z "$UNIVERSE_EXIST" ]]
+then
+    echo universe repo already enabled
+else
+    echo y | sudo add-apt-repository universe
+fi
+
+if [ ! -z "$CHROME_EXIST" ]
+then
+    echo chrome repo already enabled
+else
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+fi
+
 sudo apt-get update
-sudo apt-get install vim git zsh curl gnome-tweaks powerline-fonts vlc sudo apt-get install python3-pip -y
-sudo pip install powerline-status
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install -f vim git zsh curl gnome-tweaks fonts-powerline fonts-hack-ttf vlc python3-pip -y
+
+cp ./.zshrc ~/.zshrc
+
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P ~/Downloads/zsh/
+sh ~/Downloads/zsh/install.sh --unattended
+
+chsh -s $(which zsh)
+
 git clone https://github.com/vinceliuice/Orchis-theme.git ~/Downloads/orchis/
 ~/Downloads/orchis/install.sh -t purple
 
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 wget https://prerelease.keybase.io/keybase_amd64.deb -P ~/Downloads/
 
@@ -23,5 +52,3 @@ sudo snap install --classic code
 sudo snap install --classic godot
 sudo snap install --classic discord
 sudo snap install --classic spotify
-
-cp ./.zshrc ~/.zshrc
